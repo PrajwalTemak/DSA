b8360b1044f8061b77f9b913ec11e4b54e9eb828
Prajwal <105584120+PrajwalTemak@users.noreply.github.com>
2023-10-19 15:00:21 +0530

    Create Factorial.cpp

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+	if(n==0)
+		return 0;
+	
+	if (n==1)
+		return 1;
+		
+	return n* fact(n-1);
+}
+
+int main()
+{
+	int n = 5;
+	cout<<fact(n);
+	return 0;
+}
